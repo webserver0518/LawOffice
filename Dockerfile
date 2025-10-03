d1c6413f532ee2091b440fc51f435f44cef97608
matansuliman <matan.2001@gmail.com>
2025-10-04 01:37:17 +0300

    convert to gunicorn

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,4 @@ WORKDIR /app
 COPY . .
 RUN pip install -r requirements.txt
 EXPOSE 8000
-CMD ["python", "run.py"]
+CMD ["gunicorn", "-w", "4", "-b", "0.0.0.0:8000", "run:app"]
--- run.py
+++ run.py
@@ -4,6 +4,3 @@ import os
 
 env = os.getenv('FLASK_ENV', 'development')
 app = create_app(env=env)
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8000, debug=True)
