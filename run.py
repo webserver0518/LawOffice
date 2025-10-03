7fcaad13bc33baecb949c7cd9e105189866a70d4
matansuliman <matan.2001@gmail.com>
2025-10-04 00:02:21 +0300

    add Dockerfile, docker-compose and edit server_setup.sh and run.py

--- run.py
+++ run.py
@@ -6,4 +6,4 @@ env = os.getenv('FLASK_ENV', 'development')
 app = create_app(env=env)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+    app.run(host="0.0.0.0", port=8000, debug=True)
