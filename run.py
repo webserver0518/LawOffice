f7912d47f92ecde14ea5a395a17b13052c069ac1
matansuliman <matan.2001@gmail.com>
2025-10-04 00:48:45 +0300

    all 8000

--- run.py
+++ run.py
@@ -6,4 +6,4 @@ env = os.getenv('FLASK_ENV', 'development')
 app = create_app(env=env)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=80, debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
